247d4f4c33bd27aaa66880afeeb27f251b5e28ad
and312 <andreapelaez436@gmail.com>
2025-12-03 22:35:25 -0400

    [ADD] section PageHader in AccreditationPage

--- src/presentation/pages/AccreditationPage.tsx
+++ src/presentation/pages/AccreditationPage.tsx
@@ -1,6 +1,17 @@
+import { Box } from "@mui/material"
+import accreditationHero from "../../assets/page-headers/accreditation.jpg";
+import PageHeader from "../components/containers/PageHeader";
+
 const AccreditationPage = () => {
   return (
-    <>ACREDITACION</>
+    <Box>
+      {/* Sección: Cabecera */}
+      <PageHeader
+        title="Acreditación y Calidad"
+        subtitle="Somos el primer laboratorio en Bolivia certificado bajo la norma ISO/IEC 17025, garantizando resultados confiables y reconocidos internacionalmente"
+        backgroundImage={accreditationHero}
+      />
+    </Box>
   );
 }
 
