14c78c45404fd545e8c2540681fde6f42c664c03
and312 <andreapelaez436@gmail.com>
2025-12-03 22:41:40 -0400

    [ADD] section Resultados certificados in AccreditationPage

--- src/presentation/pages/AccreditationPage.tsx
+++ src/presentation/pages/AccreditationPage.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material"
 import accreditationHero from "../../assets/page-headers/accreditation.jpg";
 import PageHeader from "../components/containers/PageHeader";
+import CallToActionSection from "../components/containers/CallToActionSection";
 
 const AccreditationPage = () => {
   return (
@@ -10,6 +11,13 @@ const AccreditationPage = () => {
         title="Acreditación y Calidad"
         subtitle="Somos el primer laboratorio en Bolivia certificado bajo la norma ISO/IEC 17025, garantizando resultados confiables y reconocidos internacionalmente"
         backgroundImage={accreditationHero}
+      />
+      {/* Sección: Resultados certificados */}
+      <CallToActionSection
+        title="¿Necesitas resultados certificados?"
+        subtitle="Solicita nuestros servicios acreditados y obtén resultados con validez internacional"
+        primaryButton={{ label: "Solicitar Análisis" }}
+        secondaryButton={{ label: "Conocer Nuestro Equipo" }}
       />
     </Box>
   );
